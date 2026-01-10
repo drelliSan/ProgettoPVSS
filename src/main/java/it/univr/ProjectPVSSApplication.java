0c00c4f08fbc917d24e178faf6a9e30fed3412c4
drelli <drellisan@gmail.com>
2026-01-10 17:31:05 +0100

    Init

---
+++ src/main/java/it/univr/ProjectPVSSApplication.java
@@ -0,0 +1,11 @@
+package it.univr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPVSSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPVSSApplication.class, args);
+    }
+}
